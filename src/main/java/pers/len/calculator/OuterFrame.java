126c4fff88f910d7e576d693a2b15633ae8f2d07
yohren00 <yohren00@gmil.com>
2019-08-11 19:20:24 +0800

    計算機第一版(C、back、+-尚未實裝)

--- src/main/java/pers/len/calculator/OuterFrame.java
+++ src/main/java/pers/len/calculator/OuterFrame.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.util.ArrayList;
 
 public class OuterFrame {
     private JFrame jframe;
     private String[] name;
     private int att[][];
+    private JLabel jl0;
     private ArrayList<JComponent> Button;
     private static long num;
     private static byte op;
@@ -31,25 +33,23 @@ public class OuterFrame {
                 GridBagConstraints.NORTH,
                 GridBagConstraints.NORTHEAST};
         //按鍵設定
-        String s[] = {"0",
-                "CE", "C", "Back", "/",
-                "7", "8", "9", "x",
-                "4", "5", "6", "-",
-                "1", "2", "3", "+",
-                "+-", "0", ".", "="};
+        String s[] = {"0", "CE", "C", "Back", "/", "x", "-", "+", "=", "+-", ".",
+                "7", "8", "9",
+                "4", "5", "6",
+                "1", "2", "3", "0"};
         this.name = s;//把s值給name，要不然會拋出null錯誤
         //各按鍵位置
         int a[][] = {{0, 0, 5, 1, 1, 1, fill[3], anchor[1]},
                 {0, 1, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 1, 1, 1, 1, 1, fill[3], anchor[0]},
                 {2, 1, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 1, 1, 1, 1, 1, fill[3], anchor[0]},
+                {3, 2, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 3, 1, 1, 1, 1, fill[3], anchor[0]},
+                {3, 4, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 5, 1, 1, 1, 1, fill[3], anchor[0]},
+                {0, 5, 1, 1, 1, 1, fill[3], anchor[0]}, {2, 5, 1, 1, 1, 1, fill[3], anchor[0]},
                 {0, 2, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 2, 1, 1, 1, 1, fill[3], anchor[0]},
-                {2, 2, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 2, 1, 1, 1, 1, fill[3], anchor[0]},
-                {0, 3, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 3, 1, 1, 1, 1, fill[3], anchor[0]},
-                {2, 3, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 3, 1, 1, 1, 1, fill[3], anchor[0]},
+                {2, 2, 1, 1, 1, 1, fill[3], anchor[0]}, {0, 3, 1, 1, 1, 1, fill[3], anchor[0]},
+                {1, 3, 1, 1, 1, 1, fill[3], anchor[0]}, {2, 3, 1, 1, 1, 1, fill[3], anchor[0]},
                 {0, 4, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 4, 1, 1, 1, 1, fill[3], anchor[0]},
-                {2, 4, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 4, 1, 1, 1, 1, fill[3], anchor[0]},
-                {0, 5, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 5, 1, 1, 1, 1, fill[3], anchor[0]},
-                {2, 5, 1, 1, 1, 1, fill[3], anchor[0]}, {3, 5, 1, 1, 1, 1, fill[3], anchor[0]}};
+                {2, 4, 1, 1, 1, 1, fill[3], anchor[0]}, {1, 5, 1, 1, 1, 1, fill[3], anchor[0]}};
         this.att = a;//把a值給att，要不然會拋出null錯誤
 
     }
@@ -65,10 +65,9 @@ public class OuterFrame {
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//關閉程式
 
         //新增顯示示框到版面
-        Button = new ArrayList<JComponent>(21);
-        JLabel jl0 = new JLabel(name[0]);
+        Button = new ArrayList<JComponent>(20);
+        jl0 = new JLabel(name[0]);
         jl0.setFont(new Font("標楷體", Font.BOLD, 20));
-        jl0.setBackground(new Color(240,220,190));
         Button.add(jl0);
 
         //新增按鈕到版面
@@ -76,15 +75,20 @@ public class OuterFrame {
             JButton jball = new JButton(name[i]);
             jball.setFont(new Font("標楷體", Font.BOLD, 16));
             Button.add(jball);
-
         }
         //按鈕版面的配置方式
         for (int i = 0; i < Button.size(); i++) {
             addComponent(i);
         }
+        for (int i=11 ; i < 21; i++) {
+            JButton  jb = (JButton) Button.get(i);
+            jb.addActionListener(new Numb());
+        }
 
-        JButton j = (JButton) Button.get(1);
-        j.addActionListener(new OneListener());
+        for (int i=1 ; i < 11; i++) {
+            JButton  jb = (JButton) Button.get(i);
+            jb.addActionListener(new Calculatio());
+        }
 
 
         jframe.setVisible(true);//顯示視窗
@@ -109,76 +113,74 @@ public class OuterFrame {
         jframe.add(Button.get(i), gbc);
     }
 
-    //按鈕1
-    class OneListener implements ActionListener {
+    //觸發按鈕
+    class Numb implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            long result;//存放由字串轉成的數值
-            JLabel btn = (JLabel) e.getSource();
-            //處理數值1-9
-            for (int i = 0; i <= 9; i++) {
-                if (btn.equals(Button.get(i))) {
+            JButton btn = (JButton) e.getSource();
+            //處理數值0-9
+            for (int i = 11; i < 21; i++) {
+                if (btn == Button.get(i)) {
                     output_digit((JButton) Button.get(i));
-                    break;
                 }
             }
-            if(btn.equals(Button.get(2))){
-                result=0L;//把儲存的結果歸0
-                num=0L;
-                op=0;
-                JLabel j = (JLabel) Button.get(0);
-                j.setText(Long.toString(num));
-            }else if(btn.equals(Button.get(16))){//加
-                save_num((JButton) Button.get(16));
-                op=1;
-            }else if(btn.equals(Button.get(14))){//減
-                save_num((JButton) Button.get(14));
-                op=2;
-            }else if(btn.equals(Button.get(10))){//乘
-                save_num((JButton) Button.get(10));
-                op=3;
-            }else if(btn.equals(Button.get(4))){//除
+        }
+    }
+    class Calculatio implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            long result;//存放由字串轉成的數值
+            JButton btn = (JButton) e.getSource();
+            if (btn == Button.get(2)) {
+                result = 0L;//把儲存的結果歸0
+                num = 0L;
+                op = 0;
+                jl0.setText(String.valueOf(num));
+            } else if (btn == Button.get(7)) {//加
+                save_num((JButton) Button.get(7));
+                op = 1;
+            } else if (btn == Button.get(6)) {//減
+                save_num((JButton) Button.get(6));
+                op = 2;
+            } else if (btn == Button.get(5)) {//乘
+                save_num((JButton) Button.get(5));
+                op = 3;
+            } else if (btn == Button.get(4)) {//除
                 save_num((JButton) Button.get(4));
-                op=4;
-            }else if(btn.equals(Button.get(20))){
-                JLabel j = (JLabel) Button.get(0);
-                result=Long.parseLong(j.getText());
+                op = 4;
+            } else if (btn == Button.get(8)) {
+                result = Long.parseLong(jl0.getText());
 
-                switch(op){
+                switch (op) {
                     case 1:
-                        num+=result;
+                        num += result;
                         break;
                     case 2:
-                        num-=result;
+                        num -= result;
                         break;
                     case 3:
-                        num*=result;
+                        num *= result;
                         break;
                     case 4:
-                        try{
-                            num/=result;
-                        }catch(ArithmeticException ae){
+                        try {
+                            num /= result;
+                        } catch (ArithmeticException ae) {
                             num = 0L;
                             result = 0L;
                             System.out.println("被除數不得為0");
                         }
                         break;
-                    default:
                 }
-                result=0L;
-                //輸出運算後的結果到顯示器
-                j.setText(Long.toString(num));
+
+//輸出運算後的結果到顯示器
+
+                jl0.setText(Long.toString(num));
             }
         }
-        //輸出數值到顯示器
-        private void output_digit (JButton btn){
-            JLabel j = (JLabel) Button.get(0);
-            j.setText(Long.toString(Long.parseLong(j.getText() + btn.getLabel())));
-        }
-        //把第一組數值儲存起來
-        private void save_num(JButton oper){
-            JLabel j = (JLabel) Button.get(0);
-            num=Long.parseLong(j.getText());
-            j.setText(Long.toString(0L));
-        }
+    }
+    private void output_digit(JButton btn){
+        jl0.setText(Long.toString(Long.parseLong(jl0.getText()+btn.getLabel())));
+    }
+    private void save_num(JButton btn){
+        num=Long.parseLong(jl0.getText());
+        jl0.setText(Long.toString(0L));
     }
 }
